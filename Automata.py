f46dafb82a8572e02e395787c1061f2d66b9a191
Bowen Shiro Husin <122879756+ShiroHusin@users.noreply.github.com>
2023-01-28 03:45:25 +1100

    Update Automata.py

--- Automata.py
+++ Automata.py
@@ -112,46 +112,45 @@ def initialize_grid(length, choice, alpha, no_of_frames):
     test_grid=np.copy(grid)
     return grid, test_grid, float(alpha), int(no_of_frames),n
 
-# Initialise the questions
-questions = Questions()
-length = questions.get_length()
-choice = questions.get_shape()
-alpha = questions.get_alpha()
-no_of_frames = questions.get_frames()
-grid, test_grid, alpha, no_of_frames,n = initialize_grid(length, choice, alpha, no_of_frames)
 
-# Initialise plots
-fig, (ax,ax2) = plt.subplots(1,2, figsize=(10,5), width_ratios=[1.8, 1])
-im = ax.imshow(grid[1:-1, 1:-1], cmap='inferno', vmin=0, vmax=1)
-ax.set_xlabel("Rows")
-ax.set_ylabel("Columns")
-xor_values = []
+class AutomataSimulation:
+    def __init__(self, grid, alpha, no_of_frames,length):
+        self.n=length
+        self.grid = grid
+        self.alpha = alpha
+        self.no_of_frames = no_of_frames
+        self.test_grid = np.copy(self.grid)
+        self.fig, (self.ax,self.ax2) = plt.subplots(1,2, figsize=(10,5), width_ratios=[1.8, 1])
+        self.im = self.ax.imshow(grid[1:-1, 1:-1], cmap='inferno', vmin=0, vmax=1)
+        self.ax.set_xlabel("Rows")
+        self.ax.set_ylabel("Columns")
+        self.xor_values = []
+        self.ani = animation.FuncAnimation(self.fig, self.update, frames=self.no_of_frames, repeat=False, interval=50)
 
-def update(frame,grid):
-    grid = apply_rules_2d(grid,  alpha)
-    im.set_data(grid[1:-1, 1:-1])
-    ax.set_title("Generation: {}".format(frame),loc="left")
-    ax.set_title("Move probability: {:.0f}%".format(alpha*100),loc="right")
-    ## Compute the xor values of the original simulation as a proxy measure of its entropy.
-    xor_result = calculate_xor_sum(grid)
-    xor_values.append(xor_result)
-    ax2.plot(xor_values, color='blue')
-    ax2.set_xlabel("Generations")
-    ax2.set_ylabel("Proxy entropy measure")
-    ax2.set_title("Heated particles: {:.1f}%".format(np.sum(grid[1:-1, 1:-1]) / (n * n) * 100), loc="center")
 
-    if not check_energy_conversion_2D(test_grid, grid) or frame > no_of_frames:
-        ani.event_source.stop()
-        sys.exit()
+    def update(self, frame):
+        grid = apply_rules_2d(self.grid,  self.alpha)
+        self.im.set_data(grid[1:-1, 1:-1])
+        self.ax.set_title("Generation: {}".format(frame),loc="left")
+        self.ax.set_title("Move probability: {:.0f}%".format(self.alpha*100),loc="right")
+        xor_result = calculate_xor_sum(grid)
+        self.xor_values.append(xor_result)
+        self.ax2.plot(self.xor_values, color='blue')
+        self.ax2.set_xlabel("Generations")
+        self.ax2.set_ylabel("Proxy entropy measure")
+        self.ax2.set_title("Heated particles: {:.1f}%".format(np.sum(grid[1:-1, 1:-1]) / (self.n * self.n) * 100), loc="center")
 
-ani = animation.FuncAnimation(fig, update, fargs=(grid,), frames=no_of_frames, repeat=False,interval=50)
-plt.show()
+        if not check_energy_conversion_2D(self.test_grid, grid) or frame > self.no_of_frames:
+            self.ani.event_source.stop()
+            sys.exit()
 
+### Standardised way of data collection
 class DataCollector:
     def __init__(self):
         pass
 
-    def collect_data(self, alphas, n_generations, n_runs):
+    def collect_data(self, alphas, n_generations, n_runs, length):
+        n=length
         results = {'alpha': [], 'xor_result': []}
         for alpha in alphas:
             for i in range(n_runs):
