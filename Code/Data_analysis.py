ac8a8ac4dd66d199b28ad5c19e61a2620718aeba
Bowen Shiro Husin <122879756+ShiroHusin@users.noreply.github.com>
2023-01-29 04:58:16 +1100

    Update Data_analysis.py

--- Code/Data_analysis.py
+++ Code/Data_analysis.py
@@ -2,18 +2,18 @@
 This file is just to compile all the data so that I can plot a graph
 """
 from Automata import DataCollector
+import numpy as np
 import matplotlib.pyplot as plt
 data_collector = DataCollector()
-alphas = [0.1, 0.2, 0.4, 0.6, 0.8, 1]
-length=250
+alphas=np.arange(0,1.05,0.05)
 n1_generations = 300
 n2_generations = 600
 n3_generations = 1000
 data_point_per_alpha = 5
 
-df1 = data_collector.collect_data(alphas, n1_generations, data_point_per_alpha,length)
-df2=  data_collector.collect_data(alphas, n2_generations, data_point_per_alpha,length)
-df3=  data_collector.collect_data(alphas, n3_generations, data_point_per_alpha,length)
+df1 = data_collector.collect_data(alphas, n1_generations, data_point_per_alpha,length=250)
+df2=  data_collector.collect_data(alphas, n2_generations, data_point_per_alpha,length=250)
+df3=  data_collector.collect_data(alphas, n3_generations, data_point_per_alpha,length=250)
 #
 # ## Group the dataframe find the averages and the standard deviation for the final plot
 df1_grouped = df1.groupby('alpha')['xor_result'].mean()
